0f3fd5e6f75fcd0149dd1f2c9995be251e12cec9
Lasse Becker <lasse@cpsh.de>
2022-04-29 15:48:58 +0200

    BigBang (first commit)

---
+++ src/typeCalculus/booleanTypes.ts
@@ -0,0 +1,59 @@
+
+type Nullish = null | undefined;
+type Primitive = number | string | boolean | Nullish;
+
+// plase notice: boolean = {true, false}
+// and the TYPE true = {true}
+// and the TYPE false = {false}
+
+export type _Null_ = null;
+export type _Undefined_ = undefined;
+export type _Null_Undefined_ = null | undefined;
+export type _Zero_ = 0;
+export type _One_ = 0;
+
+export type Empty = never;
+export type _True_ = true;
+export type _False_ = false;
+export type _True_False_ = boolean;
+
+/** returns {true} or {false} */
+export type EQ<A extends boolean, B extends boolean> = [A] extends [B] ? ([B] extends [A] ? true : false) : false
+
+/** returns {true} or {false} */
+export type ContainsTrue<X extends boolean> = X extends false ? false : true;
+/** returns {true} or {false} */
+export type ContainsFalse<X extends boolean> = X extends true ? false : true;
+/** returns {true} or {false} */
+export type ContainsBoth<X extends boolean> = X extends true ? (X extends false ? true : false) : false;
+
+
+export type IsEmtpy<X extends boolean> = EQ<X, never>; // {}
+export type IsTrue<X extends boolean> = EQ<X, true>; // {true}
+export type IsFalse<X extends boolean> = EQ<X, false>; // {false}
+export type IsBool<X extends boolean> = EQ<X, boolean>; // {true, false}
+
+
+/** returns {true} or {false} or {true, false} */
+export type Not<X extends boolean> = X extends false ? (X extends true ? boolean : true) : false;
+export type And<A extends boolean, B extends boolean> =
+    ContainsBoth<A> extends true ? boolean :
+    ContainsBoth<B> extends true ? boolean :
+    ContainsFalse<A> extends true ? false :
+    ContainsFalse<B> extends true ? false :
+    ContainsTrue<A> extends true ? true :
+    ContainsTrue<B> extends true ? true :
+    never
+export type Or<A extends boolean, B extends boolean> =
+    ContainsBoth<A> extends true ? boolean :
+    ContainsBoth<B> extends true ? boolean :
+    ContainsTrue<A> extends true ? true :
+    ContainsTrue<B> extends true ? true :
+    IsEmtpy<A> extends true ? never :
+    IsEmtpy<B> extends true ? never :
+    false
+export type XOr<A extends boolean, B extends boolean> =
+    ContainsBoth<A> extends true ? boolean :
+    ContainsBoth<B> extends true ? boolean :
+    Not<EQ<A, B>>;
+export type Equivalent<A extends boolean, B extends boolean> = Not<XOr<A, B>>;
---
+++ test/TrueNumber.test.ts
@@ -0,0 +1,191 @@
+import { Z, Z0, R, R0, Z_Pos, Z0_Pos, R_Pos, R0_Pos, Z_Neg, Z0_Neg, R_Neg, R0_Neg, TrueNumber } from "../src/numbers/integer"
+import { IsTrue } from "../src/typeCalculus/booleanTypes";
+
+function typeAssert<Var extends boolean>(x: IsTrue<Var>) { }
+
+
+type IsSubsetOf<Sub, Super> = [Sub] extends [Super] ? true : false;
+
+
+describe("TrueNumber", () => {
+    describe("Relations", () => {
+        it("R is Subset of", () => {
+            typeAssert<IsSubsetOf<R, R>>(true);
+            typeAssert<IsSubsetOf<R, R0>>(false);
+            typeAssert<IsSubsetOf<R, R_Pos>>(false)
+            typeAssert<IsSubsetOf<R, R_Neg>>(false);
+            typeAssert<IsSubsetOf<R, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<R, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<R, Z>>(false);
+            typeAssert<IsSubsetOf<R, Z0>>(false);
+            typeAssert<IsSubsetOf<R, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R, Z0_Neg>>(false);
+        })
+
+        it("R0 is Subset of", () => {
+            typeAssert<IsSubsetOf<R0, R>>(true);
+            typeAssert<IsSubsetOf<R0, R0>>(true);
+            typeAssert<IsSubsetOf<R0, R_Pos>>(false)
+            typeAssert<IsSubsetOf<R0, R_Neg>>(false);
+            typeAssert<IsSubsetOf<R0, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<R0, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<R0, Z>>(false);
+            typeAssert<IsSubsetOf<R0, Z0>>(false);
+            typeAssert<IsSubsetOf<R0, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R0, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R0, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R0, Z0_Neg>>(false);
+        })
+
+        it("R_Pos is Subset of", () => {
+            typeAssert<IsSubsetOf<R_Pos, R>>(true);
+            typeAssert<IsSubsetOf<R_Pos, R0>>(false);
+            typeAssert<IsSubsetOf<R_Pos, R_Pos>>(true);
+            typeAssert<IsSubsetOf<R_Pos, R_Neg>>(false);
+            typeAssert<IsSubsetOf<R_Pos, R0_Pos>>(false);
+            typeAssert<IsSubsetOf<R_Pos, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z0>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R_Pos, Z0_Neg>>(false);
+        })
+        it("R_Neg is Subset of", () => {
+            typeAssert<IsSubsetOf<R_Neg, R>>(true);
+            typeAssert<IsSubsetOf<R_Neg, R0>>(false);
+            typeAssert<IsSubsetOf<R_Neg, R_Pos>>(false)
+            typeAssert<IsSubsetOf<R_Neg, R_Neg>>(true);
+            typeAssert<IsSubsetOf<R_Neg, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<R_Neg, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z0>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R_Neg, Z0_Neg>>(false);
+        })
+        it("R0_Pos is Subset of", () => {
+            typeAssert<IsSubsetOf<R0_Pos, R>>(true);
+            typeAssert<IsSubsetOf<R0_Pos, R0>>(true);
+            typeAssert<IsSubsetOf<R0_Pos, R_Pos>>(true)
+            typeAssert<IsSubsetOf<R0_Pos, R_Neg>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, R0_Pos>>(true)
+            typeAssert<IsSubsetOf<R0_Pos, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z0>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R0_Pos, Z0_Neg>>(false);
+        })
+
+        it("R0_Neg is Subset of", () => {
+            typeAssert<IsSubsetOf<R0_Neg, R>>(true);
+            typeAssert<IsSubsetOf<R0_Neg, R0>>(true);
+            typeAssert<IsSubsetOf<R0_Neg, R_Pos>>(false)
+            typeAssert<IsSubsetOf<R0_Neg, R_Neg>>(true);
+            typeAssert<IsSubsetOf<R0_Neg, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<R0_Neg, R0_Neg>>(true);
+            typeAssert<IsSubsetOf<R0_Neg, Z>>(false);
+            typeAssert<IsSubsetOf<R0_Neg, Z0>>(false);
+            typeAssert<IsSubsetOf<R0_Neg, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<R0_Neg, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<R0_Neg, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<R0_Neg, Z0_Neg>>(false);
+        })
+
+        it("Z is Subset of", () => {
+            typeAssert<IsSubsetOf<Z, R>>(true);
+            typeAssert<IsSubsetOf<Z, R0>>(false);
+            typeAssert<IsSubsetOf<Z, R_Pos>>(false)
+            typeAssert<IsSubsetOf<Z, R_Neg>>(false);
+            typeAssert<IsSubsetOf<Z, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<Z, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<Z, Z>>(true);
+            typeAssert<IsSubsetOf<Z, Z0>>(false);
+            typeAssert<IsSubsetOf<Z, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<Z, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<Z, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<Z, Z0_Neg>>(false);
+        })
+
+        it("Z0 is Subset of", () => {
+            typeAssert<IsSubsetOf<Z0, R>>(true);
+            typeAssert<IsSubsetOf<Z0, R0>>(true);
+            typeAssert<IsSubsetOf<Z0, R_Pos>>(false)
+            typeAssert<IsSubsetOf<Z0, R_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<Z0, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0, Z>>(true);
+            typeAssert<IsSubsetOf<Z0, Z0>>(true);
+            typeAssert<IsSubsetOf<Z0, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<Z0, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<Z0, Z0_Neg>>(false);
+        })
+
+        it("Z_Pos is Subset of", () => {
+            typeAssert<IsSubsetOf<Z_Pos, R>>(true);
+            typeAssert<IsSubsetOf<Z_Pos, R0>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, R_Pos>>(true)
+            typeAssert<IsSubsetOf<Z_Pos, R_Neg>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<Z_Pos, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, Z>>(true);
+            typeAssert<IsSubsetOf<Z_Pos, Z0>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, Z_Pos>>(true);
+            typeAssert<IsSubsetOf<Z_Pos, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<Z_Pos, Z0_Neg>>(false);
+        })
+
+        it("Z_Neg is Subset of", () => {
+            typeAssert<IsSubsetOf<Z_Neg, R>>(true);
+            typeAssert<IsSubsetOf<Z_Neg, R0>>(false);
+            typeAssert<IsSubsetOf<Z_Neg, R_Pos>>(false)
+            typeAssert<IsSubsetOf<Z_Neg, R_Neg>>(true);
+            typeAssert<IsSubsetOf<Z_Neg, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<Z_Neg, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<Z_Neg, Z>>(true);
+            typeAssert<IsSubsetOf<Z_Neg, Z0>>(false);
+            typeAssert<IsSubsetOf<Z_Neg, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<Z_Neg, Z_Neg>>(true);
+            typeAssert<IsSubsetOf<Z_Neg, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<Z_Neg, Z0_Neg>>(false);
+        })
+        it("Z0_Pos is Subset of", () => {
+            typeAssert<IsSubsetOf<Z0_Pos, R>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, R0>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, R_Pos>>(true)
+            typeAssert<IsSubsetOf<Z0_Pos, R_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0_Pos, R0_Pos>>(true)
+            typeAssert<IsSubsetOf<Z0_Pos, R0_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0_Pos, Z>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, Z0>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, Z_Pos>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, Z_Neg>>(false);
+            typeAssert<IsSubsetOf<Z0_Pos, Z0_Pos>>(true);
+            typeAssert<IsSubsetOf<Z0_Pos, Z0_Neg>>(false);
+        })
+
+        it("Z0_Neg is Subset of", () => {
+            typeAssert<IsSubsetOf<Z0_Neg, R>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, R0>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, R_Pos>>(false)
+            typeAssert<IsSubsetOf<Z0_Neg, R_Neg>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, R0_Pos>>(false)
+            typeAssert<IsSubsetOf<Z0_Neg, R0_Neg>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, Z>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, Z0>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, Z_Pos>>(false);
+            typeAssert<IsSubsetOf<Z0_Neg, Z_Neg>>(true);
+            typeAssert<IsSubsetOf<Z0_Neg, Z0_Pos>>(false);
+            typeAssert<IsSubsetOf<Z0_Neg, Z0_Neg>>(true);
+        })
+
+    })
+
+})
