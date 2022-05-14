155bccb31a4a5b704c4ac9711f5b6cd794ebea65
Lasse Becker <lasse@cpsh.de>
2022-05-14 10:20:27 +0200

    delete builds

--- build/typeCalculus/ArrayOperations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const type_testing_1 = require("./type-testing");
-(0, type_testing_1.typeAssert)(true);
-(0, type_testing_1.typeAssert)(false);
-(0, type_testing_1.typeAssert)(true);
